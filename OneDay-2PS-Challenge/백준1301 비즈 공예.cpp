cb1b92f971c1e34149a0d6d97ff838565ee59bf6
KimSeongHeon <rlat302@naver.com>
2020-06-04 23:17:46 +0900

    2020.06.02
    1. 비즈 공예
    2. 행성 탐사

---
+++ OneDay-2PS-Challenge/백준1301 비즈 공예.cpp
@@ -0,0 +1,7 @@
+/*2020/06/02 첫번째 문제
+백준1301 비즈 공예*/
+#include<iostream>
+using namespace std;
+int main() {
+	return 0;
+}
---
+++ OneDay-2PS-Challenge/백준5549 행성탐사.cpp
@@ -0,0 +1,7 @@
+/*2020/06/02 두번째 문제
+백준5549 행성탐사*/
+#include<iostream>
+using namespace std;
+int main() {
+	return 0;
+}
