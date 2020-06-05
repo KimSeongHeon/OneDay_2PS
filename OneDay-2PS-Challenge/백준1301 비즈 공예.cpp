53b06c60addd86c9e6a5d16c217b50c52825a14f
KimSeongHeon <rlat302@naver.com>
2020-06-05 21:26:52 +0900

    2020.06.02
    1. 백준1301 비즈공예(DP)
    2/ 백준5549 행성탐사(구현)

--- OneDay-2PS-Challenge/백준1301 비즈 공예.cpp
+++ OneDay-2PS-Challenge/백준1301 비즈 공예.cpp
@@ -1,7 +1,61 @@
 /*2020/06/02 첫번째 문제
-백준1301 비즈 공예*/
+백준1303 비즈공예(DP)*/
+//DP 아니고서는 경우의 수가 굉장히 많아서 풀 수가 없음
+//처음에 문제를 잘못보고 10미만인줄 알고 각각의 개수를 10진수로 변환하여 풀기 시도
+//10이 있기떄문에 10진수로 하면 안됨.
+//7차원 배열을 사용하여 풀었음
 #include<iostream>
+#include<vector>
+#include<memory.h>
 using namespace std;
-int main() {
+int n;
+vector<int> v;
+long long cache[11][11][11][11][11][7][7];
+long long cal(int one, int two, int three, int four, int five, int before, int bbefore){
+	if (one == 0 && two == 0 && three == 0 && four == 0 && five == 0) {
+		return 1;
+	}
+	if (one < 0 || two < 0 || three < 0 || four < 0 || five < 0) {
+		return 0;
+	}
+	long long &ret = cache[one][two][three][four][five][before][bbefore];
+	if (ret != -1) {
+		return ret;
+	}
+	ret = 0;
+	if (one >= 1 && before != 1 && bbefore != 1) {
+		ret += cal(one - 1, two, three, four, five, 1, before);
+	}
+
+	if (two >= 1 && before != 2 && bbefore != 2) {
+		ret += cal(one, two - 1, three, four, five, 2, before);
+	}
+
+	if (three >= 1 && before != 3 && bbefore != 3) {
+		ret += cal(one, two, three - 1, four, five, 3, before);
+	}
+
+	if (four >= 1 && before != 4 && bbefore != 4) {
+		ret += cal(one, two, three, four - 1, five, 4, before);
+	}
+
+	if (five >= 1 && before != 5 && bbefore != 5) {
+		ret += cal(one, two, three, four, five - 1, 5, before);
+	}
+	return ret;
+}
+
+int main(){
+	memset(cache, -1, sizeof(cache));
+	cin >> n;
+	int element;
+	for (int i = 0; i < n; i++) {
+		cin >> element;
+		v.push_back(element);
+	}
+	for (int i = n; i < 5; i++) {
+		v.push_back(0);
+	}
+	cout << cal(v[0], v[1], v[2], v[3], v[4], -1, -1) << endl;
 	return 0;
 }
--- OneDay-2PS-Challenge/백준5549 행성탐사.cpp
+++ OneDay-2PS-Challenge/백준5549 행성탐사.cpp
@@ -1,7 +1,80 @@
 /*2020/06/02 두번째 문제
-백준5549 행성탐사*/
+백준5549 행성탐사(구현)*/
+//시간초과 안나게 구현하는 것이 핵심
+//입력 받을 때마다 이전의 값들을 활용하여 J,O,i의 개수를 세야함
+//그리고 그 값을 이용하여 수학적으로 계산하면 답을 구할 수 있음.
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
+struct area {
+	int jungle;
+	int ocean;
+	int ice;
+};
+area map[1001][1001];
+area row;
+area col[1001];
+area all;
+void cal(int x, int y,char character) {
+	map[x][y] = map[x - 1][y - 1];
+	map[x][y].jungle += row.jungle;
+	map[x][y].ocean += row.ocean;
+	map[x][y].ice += row.ice;
+	map[x][y].jungle += col[y].jungle;
+	map[x][y].ocean += col[y].ocean;
+	map[x][y].ice += col[y].ice;
+	if (character == 'J') {
+		map[x][y].jungle++;
+		row.jungle++;
+		col[y].jungle++;
+		all.jungle++;
+	}
+	else if (character == 'O') {
+		map[x][y].ocean++;
+		row.ocean++;
+		col[y].ocean++;
+		all.ocean++;
+	}
+	else if (character == 'I') {
+		map[x][y].ice++;
+		row.ice++;
+		col[y].ice++;
+		all.ice++;
+	}
+	//cout << map[x][y].jungle << map[x][y].ocean << map[x][y].ice << endl;
+}
+area width(int x1, int y1, int x2, int y2) {
+	area all = map[x2][y2];
+	area sub1 = map[x2][y1 - 1];
+	area sub2 = map[x1 - 1][y2];
+	area coincide = map[x1 - 1][y1 - 1];
+	area ret = { 0,0,0 };
+	ret.jungle = all.jungle - sub1.jungle - sub2.jungle + coincide.jungle;
+	ret.ocean = all.ocean - sub1.ocean - sub2.ocean + coincide.ocean;
+	ret.ice = all.ice - sub1.ice - sub2.ice + coincide.ice;
+	return ret;
+}
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m,k;
+	cin >> n >> m >> k;
+	//for (int i = 0; i <= n; i++) map[i][0] = { 0,0,0 };
+	//for (int j = 0; j <= n; j++) map[0][j] = { 0,0,0 };
+	for (int i = 1; i <= n; i++) {
+		string s;
+		cin >> s;
+		row = {0,0,0};
+		for (int j = 1; j <= m; j++) {
+			cal(i, j, s[j-1]);
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		area answer = width(x1, y1, x2, y2);
+		cout << answer.jungle << " " << answer.ocean << " " << answer.ice << "\n";
+	}
 	return 0;
 }
